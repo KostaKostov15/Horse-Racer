2e65ce20c0d2b49a3ef82a8faec5eaf056f35235
KostaKostov15 <kosta.kostov15@gmail.com>
2023-07-26 00:59:45 +0300

    added profile component html

--- client/src/app/feature/user/profile/profile.component.ts
+++ client/src/app/feature/user/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  constructor(public authService: AuthService) {}
 }
